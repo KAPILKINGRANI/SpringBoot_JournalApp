2b02e59c57010f0cf1bc874a58ccd3502bc8c187
Kapil <kingranikapil3@gmail.com>
2025-03-24 19:54:07 +0530

    Added Project Lombok In The Project To Reduce Boiler Plate Of Getters And Setters In The Entity class

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +11,7 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @Document(collection = "journalEntries")
+@Data
 public class JournalEntry {
 
 
@@ -16,29 +20,4 @@ public class JournalEntry {
     private String content;
     private LocalDate date;
 
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() { return content; }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDate getDate() { return date;}
-
-    public void setDate(LocalDate date) { this.date = date;}
 }
